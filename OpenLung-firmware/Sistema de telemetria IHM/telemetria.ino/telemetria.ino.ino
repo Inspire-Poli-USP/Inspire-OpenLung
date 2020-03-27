42f8a7d00b7f852b00c5d03d080d1ca9b2f97088
kinife <kini.fe@live.com>
2020-03-27 17:53:24 -0300

    Readequação do código
    Com base nos dados médico obtidos.

--- OpenLung-firmware/Sistema de telemetria IHM/telemetria.ino/telemetria.ino.ino
+++ OpenLung-firmware/Sistema de telemetria IHM/telemetria.ino/telemetria.ino.ino
@@ -13,18 +13,22 @@ LiquidCrystal lcd(32, 31, 45, 44, 43, 42);
 
 int buzzer = 50;                                                     //Saída buzzer
 int select = 53;                                                     // Botão de entra
-int volMed=999;                                                      // Variavel de teste para volume de ar medido pelo sensor do equipamento
+//int volMed=999;                                                      // Variavel de teste para volume de ar medido pelo sensor do equipamento
 int offsetVol=5.55;                                                  // Offset de tolerancia de ar
 float tIns;
 float rInv;
+float blend;
+int fr;
 int vol;
 int count = 0;
+
+
 
 float inspira() {
   digitalWrite(buzzer, LOW);
   int pot = analogRead(A0);                                          // Le o potenciometro 1 - referente a velocidade
-  float valorPot = map(pot, 0, 1023, 0.0, 70.0);                      // Converte o sinal lido pelo potenciometro (0~1023), para o range (vMin~vMax)
-  float potToIns = valorPot / 10.0;
+  float valorPot = map(pot, 0, 1023, 0.0, 200.0);                      // Converte o sinal lido pelo potenciometro
+  float potToIns = valorPot / 100.0;
   return potToIns;
 }
 
@@ -32,7 +36,7 @@ float inspira() {
 float invies() {
   digitalWrite(buzzer, LOW);
   int pot = analogRead(A0);                                          // Le o potenciometro 1 - referente a velocidade
-  float valorPot = map(pot, 0, 1023, 0.0, 70.0);                      // Converte o sinal lido pelo potenciometro (0~1023), para o range (vMin~vMax)
+  float valorPot = map(pot, 0, 1023, 0.0, 70.0);                      // Converte o sinal lido pelo potenciometro
   float potToInv = valorPot / 10.0;
   return potToInv;
 }
@@ -40,8 +44,23 @@ float invies() {
 int volume() {
   digitalWrite(buzzer, LOW);
   int pot = analogRead(A0);                                          // Le o potenciometro 1 - referente a velocidade
-  int vol = map(pot, 0, 1023, 0.0, 1000);                      // Converte o sinal lido pelo potenciometro (0~1023), para o range (vMin~vMax)
-  return vol;
+  int potToVol = map(pot, 0, 1023, 0.0, 1000);                      // Converte o sinal lido pelo potenciometro
+  return potToVol;
+}
+
+int frResp() {
+  digitalWrite(buzzer, LOW);
+  int pot = analogRead(A0);                                          // Le o potenciometro 1 - referente a velocidade
+  int potToFr = map(pot, 0, 1023, 0, 150);                      // Converte o sinal lido pelo potenciometro
+  return potToFr;
+}
+
+float blender() {
+  digitalWrite(buzzer, LOW);
+  int pot = analogRead(A0);                                          // Le o potenciometro 1 - referente a velocidade
+  float valorPot = map(pot, 0, 1023, 0.00, 100.00);                      // Converte o sinal lido pelo potenciometro
+  float potToBlend = valorPot / 100.0;
+  return potToBlend;
 }
 
 void setup()
@@ -56,71 +75,87 @@ void loop()
 
 {
   if (count == 0) {
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("CITI-OpenLung:");
     lcd.setCursor(0, 1);
     lcd.print("Press Enter");
     if (digitalRead(select) == HIGH) count ++;
   }
-
+  
+  else if (count == 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print("Blender (O2):");
+    blend = blender();
+    lcd.setCursor(0, 1);
+    lcd.print(blend);
+    if (digitalRead(select) == HIGH) count++;
+  }
 
   else if (count == 2) {
     //delay(200);
+    lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("T inspiracao:");
-    tIns = inspira();
+    lcd.print("Volume (ml):");
+    vol = volume();
     lcd.setCursor(0, 1);
-    lcd.print(tIns);
+    lcd.print(vol);
     if (digitalRead(select) == HIGH) count ++;
   }
 
   else if (count == 3) {
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.clear();
-    lcd.print("Invies:");
-    rInv = invies();
+    lcd.print("T. Insp (s):");
+    tIns = inspira();
     lcd.setCursor(0, 1);
-    lcd.print(rInv);
+    lcd.print(tIns);
     if (digitalRead(select) == HIGH) count++;
   }
-
-  else if (count == 1) {
-    lcd.setCursor(0, 0);
-    lcd.clear();
-    lcd.print("Volume:");
-    vol = volume();
-    lcd.setCursor(0, 1);
-    lcd.print(vol);
-    if (digitalRead(select) == HIGH) count++;
-  }
-
 
   else if (count == 4) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print("F. Resp (R/min):");
+    fr = frResp();
+    lcd.setCursor(0, 1);
+    lcd.print(fr);
+    if (digitalRead(select) == HIGH) count++;
+  }
+
+
+
+
+  else if (count == 5) {
 
     digitalWrite(buzzer, LOW);
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("ml:");                       //volume calculado pelo medico
-    lcd.setCursor(3, 0);
-    lcd.print(vol);
+    lcd.print("FO2:");                       //volume calculado pelo medico
+    lcd.setCursor(4, 0);
+    lcd.print(blend);
 
     lcd.setCursor(9, 0);
     lcd.print("ml:");                       //volume medido por ciclo
     lcd.setCursor(12, 0);
     //lcd.print(analogRead(volMed));       //entrada volume sensor              
-    lcd.print(volMed);                     //entrada volume sensor
+    lcd.print(vol);                     //entrada volume sensor
     
     lcd.setCursor(0, 1);
-    lcd.print("ri:");                     //relação invies
-    lcd.setCursor(3, 1);
-    lcd.print("1:");                       //entrada relação invies medico
+    lcd.print("TI(s):");                     //relação invies
     lcd.setCursor(5, 1);
-    lcd.print(rInv);                       //entrada relação invies medico
+    //lcd.print("1:");                       //entrada relação invies medico
+    //lcd.setCursor(5, 1);
+    lcd.print(tIns);                       //entrada relação invies medico
 
     lcd.setCursor(10, 1);
-    lcd.print("t:");                     //tempo de inspiração
-    lcd.setCursor(12, 1);
-    lcd.print(tIns);                     //entrada tempo de inspiração medico
+    lcd.print("FR:");                     //tempo de inspiração
+    lcd.setCursor(13, 1);
+    lcd.print(fr);                     //entrada tempo de inspiração medico
 
 
     /////////////////////condição de erro volumetrico/////////////////////
